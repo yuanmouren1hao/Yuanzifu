6fdbb7a664b75b08ae0c7be9be12014b07bcfc79
yuanmouren1hao <342834599@qq.com>
2016-03-31 22:22:33 +0800

    添加必要文件和修改，还是静态文件

---
+++ task/index.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Task-元字符科技</title>
+</head>
+<body>
+	<style>
+iframe {
+	width: 100%;
+	height: 100%;
+	padding: 0px;
+	margin: 0px;
+}
+
+html,body {
+	padding: 0;
+	margin: 0;
+	width: 100%;
+	height: 100%;
+}
+</style>
+
+	<iframe frameborder="0" src="http://121.40.76.106/wss/index.php"
+		scrolling="no"></iframe>
+
+</body>
+</html>
